e31398d833dd4d52c63498726b984dc87cbc66d8
henrychen11 <henrychen1@gmail.com>
2018-01-15 19:31:21 -0800

    added new char count

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -5,17 +5,22 @@ function caesarCipher(s, k) {
     // Complete this function
     let result = '';
     console.log("input", s, k);
-    console.log("a", s.charCodeAt("a") );
+
+
     for (let i=0; i<s.length; i++){
-        let code = s.charCodeAt(i);
-        let new_key = code + k;
-        if (code >= 65){
-            console.log("current", s.charCodeAt(i));
-            console.log("new", String.fromCharCode(new_key % 109));
-            result += String.fromCharCode(new_key);
-        } else {
-            result += s[i];
-        }
+      let key = s[i].charCodeAt(0);
+      let newKey = key + k;
+
+      if (key < 65 || key > 90) {
+         result += String.fromCharCode(key);  // Return un-converted character
+       }
+       //N = ASCII 78, if the character code is less than 78, shift forward 13 places
+       else if (key < 78) {
+         result += String.fromCharCode(newKey);
+       } else {
+         // Otherwise shift the character 13 places backward
+         result += String.fromCharCode(newKey % 78);
+       }
     }
     return result;
 
