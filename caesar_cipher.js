65be90e2ba0a2e00d8904585c580c084a5d1ee0f
henrychen11 <henrychen1@gmail.com>
2018-01-15 19:48:16 -0800

    udpated ACII count

--- caesar_cipher.js
+++ caesar_cipher.js
@@ -8,18 +8,18 @@ function caesarCipher(s, k) {
 
 
     for (let i=0; i<s.length; i++){
-      let key = s[i].charCodeAt(0);
-      let newKey = key + k;
+      let key = s[i].charCodeAt();
+      let newKey = (key - "a".charCodeAt() + k) % 26 + "a".charCodeAt();
 
-      if (key < 65 || key > 90) {
+      if (key >= 'a'.charCodeAt(0) && key <= 'z'.charCodeAt(0)) {
          result += String.fromCharCode(key);  // Return un-converted character
        }
        //N = ASCII 78, if the character code is less than 78, shift forward 13 places
-       else if (key < 78) {
+       else if (key >= 'A'.charCodeAt(0) && key <= 'Z'.charCodeAt(0)) {
          result += String.fromCharCode(newKey);
        } else {
          // Otherwise shift the character 13 places backward
-         result += String.fromCharCode(newKey % 78);
+         result += String.fromCharCode(newKey);
        }
     }
     return result;
