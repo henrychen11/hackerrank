f13bb28e83186db18bbe649a602faa182ef6690e
henrychen11 <henrychen1@gmail.com>
2018-01-21 11:54:39 -0800

    add and remove function

--- linked_list.js
+++ linked_list.js
@@ -9,12 +9,69 @@ class linkedList {
     return this.count;
   }
 
+  addFirst(data){
+    const node = {
+      data: data,
+      next: null
+    };
+    //save the first node
+    const temp = this.head;
+
+    //set new node to head
+    this.head = node;
+
+    //add the rest of the nodes (linkedlists) after first node
+    this.head.next = temp;
+
+    //increment the length
+    this.count += 1;
+
+    //if first node, point to itself as well
+    if (this.count === 1){
+      this.tail = this.head;
+    }
+  }
+
   addLast(data){
     //this creates a new node
     const node = {
       data: data,
       next: null
     };
+    // the "this" is referring to the LinkedList
+    if (this.count === 0){
+      //If this is the first node, then assign to head
+      this.head = node;
+    } else {
+      //If not, then link it to the last node
+      this.tail.next = node;
+    }
+    this.tail = node;
+    //increments the length of the list
+    this.count += 1;
+  }
+
+  removeFirst(data){
+    if (this.count > 0){
+      //the head should point to the second element
+      this.head = this.head.next;
+      this.count -= 1;
+
+      //set tail to null if list is empty
+      if (this.count === 0){
+        this.tail = null;
+      }
+    }
+  }
+
+  removeLast(data){
+    if (this.count > 0){
+      //if there is only 1 node in list, then set null to head and tail
+      if(this.count === 1){
+        this.head = null;
+        this.tail = null;
+      }
+    } else
   }
 
 }
