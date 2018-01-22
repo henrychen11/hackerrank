85771c9deab5ae43667c3e1f8409b6fa329eeb7c
henrychen11 <henrychen1@gmail.com>
2018-01-21 23:26:37 -0800

    added contains function

--- trees.js
+++ trees.js
@@ -22,13 +22,19 @@ class Node {
   }
 
   contains(value){
-    if (value === this.data){
-      return true;
-    } else if (value < this.data){
-      if (this.left === null){
-        return false;
+    let current = this.root;
+    
+    while(current){
+      if (this.data === current.data){
+        return true;
+      }
+      if (this.data < current.data){
+        current = current.left;
+      } else {
+        current = current.right;
       }
     }
+    return false;
   }
 
 }
