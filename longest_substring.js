dd2fc4ce03aeeb723ecd91ba279af1bca2fc9a75
henrychen11 <henrychen1@gmail.com>
2018-01-22 14:08:16 -0800

    added longest substring

--- longest_substring.js
+++ longest_substring.js
@@ -3,21 +3,22 @@ let b = "bbbbb"; // "b"
 let c = "pwwkew"; // "wke"
 
 var lengthOfLongestSubstring = function(s) {
-    console.log("input", s);
-    let result = [];
-    for (let i=0; i<s.length; i++){
-      let start = i;
-      let end = 1;
-      if (result.includes(s[start]) === false){
-          result.push(s[i]);
-          end += 1;
-      } else {
-        start = end;
-        end = 0;
-
-      }
+  console.log("input", s);
+  let max = 0;
+  let start = 0;
+  let hash = {};
+  for (let i=0; i<s.length; i++){
+    //pulls the current letter
+    let current = s.charAt(i);
+    let keys = Object.keys(hash);
+    if (keys.includes(current) && hash[current] >= start){
+      start = hash[current] + 1;
     }
-    return result;
+    hash[current] = i;
+    max = Math.max(max, i - start + 1);
+  }
+  console.log(hash);
+  return max;
 };
 
 console.log(lengthOfLongestSubstring(a));
