969f8d2e4a561ce730c7fcf6c512a212bd28ef19
henrychen11 <henrychen1@gmail.com>
2018-01-30 11:40:14 -0800

    updated alternating char

--- alternating_characters.js
+++ alternating_characters.js
@@ -6,7 +6,16 @@ let e = 'AAABBB'; //4
 
 function alternatingCharacters(s){
     // Complete this function
-    if (s[0] === 'A')
+    let count = 0;
+    let lastChar = s.charAt(0);
+    for (let i=1; i<s.length; i++){
+      if (lastChar === s.charAt(i)){
+        count += 1;
+      } else {
+        lastChar = s.charAt(i);
+      }
+    }
+    return count;
 }
 
 console.log(alternatingCharacters(a));
