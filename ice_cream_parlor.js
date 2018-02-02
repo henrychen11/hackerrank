e0eb4638cda40b2516dfca0c6bc386b985c0c115
henrychen11 <henrychen1@gmail.com>
2018-02-01 21:23:34 -0800

    added 771 jewels and stones

--- ice_cream_parlor.js
+++ ice_cream_parlor.js
@@ -2,9 +2,9 @@ let m = 4;
 let arr = [1,4,5,3,2];
 
 
-function icecreamParlor(m, arr) {
+function icecreamParlor(a, b) {
     // Complete this function
-    console.log("input", m, arr);
+    
 
 }
 
---
+++ jewels_and_stones.js
@@ -0,0 +1,4 @@
+
+var numJewelsInStones = function(J, S) {
+
+};
