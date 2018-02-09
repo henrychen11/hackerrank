06d83972785619fdbc2a524fb4b466e15f319abb
henrychen11 <henrychen1@gmail.com>
2018-02-08 22:05:29 -0800

    updated matrix

--- toeplitz_matrix.js
+++ toeplitz_matrix.js
@@ -1,7 +1,13 @@
-var isToeplitzMatrix = function(matrix) {
-    console.log(matrix);
-    for(let i=0; i<matrix.length; i++){
-        if (matrix[0][0] === matrix[1][1] === matrix[])
-    }
+let test = [[1,2,3,4],[5,1,2,3],[9,5,1,2]];
 
+var isToeplitzMatrix = function(matrix) {
+    // console.log(matrix);
+    for(let i=0; i<matrix.length; i++){
+      for (let j=0; j<matrix[0].length; j++){
+        if (matrix[i][j] !== matrix[i + 1][j + 1]) return false;
+      }
+    }
+    return true;
 };
+
+console.log(isToeplitzMatrix(test));
