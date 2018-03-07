ac00ddc5d7bb99b44918e84453f12c97ca2f3955
henrychen11 <henrychen1@gmail.com>
2018-03-06 22:32:08 -0800

    updated

--- permutations2.js
+++ permutations2.js
@@ -1,6 +1,6 @@
 function permutations(array) {
     const results = new Array();
-    permutations(a, 0, results);
+    permutationsHelper(array, 0, results);
     return results;
 }
 
@@ -8,18 +8,18 @@ function permutationsHelper(array, start, result){
     if (start >= array.length) {
         result.push(array.slice());
     } else {
-        for (let i=0; i < array.length; i++){
+        for (let i = start; i < array.length; i++){
             swap(array, start, i);
-            permutations(a, start + 1, result);
+            permutationsHelper(array, start + 1, result);
             swap(array, start, i);
         }
     }
 }
 
 function swap(array, idx1, idx2) {
-    let temp = a[idx1];
-    a[idx1] = a[idx2];
-    a[idx2] = temp;
+    let temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
 }
 
 const input = [1,2,3];
