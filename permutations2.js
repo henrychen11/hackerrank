e8e4a9b58d30202655d1a2f4a7bb8581a3983511
henrychen11 <henrychen1@gmail.com>
2018-03-06 19:57:07 -0800

    updated

--- permutations2.js
+++ permutations2.js
@@ -1,3 +1,11 @@
-function permutations(string){
-    
+function permutations(array, start, result){
+    if (start >= a.length) {
+        
+    }
+}
+
+function swap(array, idx1, idx2) {
+    let temp = a[idx1];
+    a[idx1] = a[idx2];
+    a[idx2] = temp;
 }
