de915946c1c2652aa4fdc00a663dd377410e552b
Henry Chen <henrychen1@gmail.com>
2018-03-14 21:43:13 -0700

    updated

--- coin_change.js
+++ coin_change.js
@@ -11,7 +11,7 @@ function CoinChange(amount, coins){
       }
     }
   }
-
+  console.log(combinations)
   return combinations[amount];
 }
 
@@ -26,11 +26,43 @@ function coinChange2(coins, amount){
       }
     }
   }
-
+  console.log(combinations)
   return combinations[amount];
 }
 
-let a = 12;
+
+function waysToReturnMemoize(amount, denominations) {
+  // intialize an array of zeros with indices up to amount
+  var waysOfDoingNcents = [];
+  for (var i = 0; i <= amount; i++) {
+      waysOfDoingNcents[i] = 0;
+  }
+  // there is 1 way to renturn 0 cents
+  waysOfDoingNcents[0] = 1;
+
+  for (var j = 0; j < denominations.length; j++) {
+      // we can only start returning change with coins in our denominations
+      var coin = denominations[j];
+      
+      // we start bottom up, each time reducing change amout with curr coin.
+      for (var higherAmount = coin; higherAmount <= amount; higherAmount++) {
+          var higherAmountRemainder = higherAmount - coin;
+          // ways to create change is equivalent to reducing the problem to a known problem
+          // and gaining all the ways to solve for smaller problems
+          waysOfDoingNcents[higherAmount] += waysOfDoingNcents[higherAmountRemainder];
+      }
+  }
+
+  return waysOfDoingNcents[amount];
+}
+
+var denominations = [1, 2, 3];
+var amount = 4;
+console.log(waysToReturnChange(denominations, denominations.length - 1, amount));
+console.log(waysToReturnMemoize(amount,denominations));
+
+
+let a = 11;
 let b = [1,2,5];
 
 console.log(CoinChange(a,b));
--- test.js
+++ test.js
@@ -1,25 +1,16 @@
-'use strict';
+var twoSum = function(nums, target) {
 
-const processData = input => {
-    let lines = input.split('\n');
-    let text = lines[1];
-    let shift = parseInt(lines[2]);
-    let encrypted = '';
-    for(let i = 0; i < text.length; i++) {
-        if(text.charCodeAt(i) >= 'a'.charCodeAt(0) && text.charCodeAt(i) <= 'z'.charCodeAt(0)) {
-            encrypted += String.fromCharCode((text.charCodeAt(i) - 'a'.charCodeAt(0) + shift) % 26 + 'a'.charCodeAt(0))
-        } else if(text.charCodeAt(i) >= 'A'.charCodeAt(0) && text.charCodeAt(i) <= 'Z'.charCodeAt(0)) {
-            encrypted += String.fromCharCode((text.charCodeAt(i) - 'A'.charCodeAt(0) + shift) % 26 + 'A'.charCodeAt(0))
-        } else {
-            encrypted += text.charAt(i);
-        }
+    let hashMap = {},
+        results = []
+    
+    for (let i = 0; i < nums.length; i++){
+      if (hashMap[nums[i]]){
+        return [hashMap[nums[i]], nums[i]];
+      }else{
+        hashMap[target - nums[i]] = i
+      }
     }
-    console.log(encrypted);
-};
-
-process.stdin.resume();
-process.stdin.setEncoding("ascii");
-
-var _input = "";
-process.stdin.on("data", input => _input += input);
-process.stdin.on("end", () => processData(_input));
+    return results;
+      
+      console.log(hashMap);
+  };
