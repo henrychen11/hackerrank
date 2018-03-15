4139d173a827dc401266d60501dc2e28178c444b
Henry Chen <henrychen1@gmail.com>
2018-03-14 22:10:03 -0700

    updated

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
 var twoSum = function(nums, target) {
-
-    let hashMap = {},
-        results = []
+    let hashMap = {};
     
     for (let i = 0; i < nums.length; i++){
-      if (hashMap[nums[i]]){
-        return [hashMap[nums[i]], nums[i]];
-      }else{
+      if (hashMap[nums[i]] !== undefined ){
+          console.log('found', hashMap)
+        return [hashMap[nums[i]], i];
+      } else {
+        console.log('here', hashMap)
         hashMap[target - nums[i]] = i
       }
     }
-    return results;
-      
       console.log(hashMap);
-  };
+};
+
+// console.log(twoSum([3,2,4], 6));
+console.log(twoSum([3,3], 6));
