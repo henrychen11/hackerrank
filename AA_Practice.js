d78574a6ca3e15c1fa78ac30d78f18a0aa15dafe
Henry Chen <henrychen1@gmail.com>
2018-04-06 20:45:26 -0700

    added aa practice

---
+++ AA_Practice.js
@@ -0,0 +1,5 @@
+// Caesar Cipher
+
+// Ping Pong 
+// Given an array, print out the 1st-3rd-5th... elements
+// Extract / pop out the element, until the array is empty
--- for_han.rb
+++ for_han.rb
@@ -1,19 +1,114 @@
-# Your last C# code is saved below:
-# In cryptography, a Caesar cipher, also known as Caesar's cipher, 
-# the shift cipher, Caesar's code or Caesar shift,
-# is one of the simplest and most widely known encryption techniques. 
-#   It is a type of substitution cipher in which each letter in the plaintext 
-#   is replaced by a letter some fixed number of positions down the alphabet. 
-#   For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. 
-#   The method is named after Julius Caesar, who used it in his private correspondence.
-# 
-# Task: Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
-# 
+# Back in the good old days, you used to be able to write a darn near
+# uncrackable code by simply taking each letter of a message and incrementing it
+# by a fixed number, so "abc" by 2 would look like "cde", wrapping around back
+# to "a" when you pass "z".  Write a function, `caesar_cipher(str, shift)` which
+# will take a message and an increment amount and outputs the encoded message.
+# Assume lowercase and no punctuation. Preserve spaces.
+#
+# To get an array of letters "a" to "z", you may use `("a".."z").to_a`. To find
+# the position of a letter in the array, you may use `Array#find_index`.
+
+def caesar_cipher(str, shift)
+  
+    list = ("a".."z").to_a
+    letters = str.split("")
+  
+    result = []
+    
+    
+    i = 0
+    
+    while i < letters.length
+      puts "here"
+      
+      if letters[i] == " "
+        result.push(" ")
+      else
+        index = list.find_index(letters[i])
+        
+        shifted_position = (index + shift) % 26  
+        
+        result.push( list[shifted_position] )
+      end
+      i += 1
+      #puts result
+    end
+    result.join
+    
+  end
+  
+  caesar_cipher("aaa", 11) #("lll")
+  
+  #caesar_cipher("zzz", 1) #("aaa")
+  
+  #caesar_cipher("catz hatz", 2) #("ecvb jcvb")
 
 
-def caesar_cipher(string, shift)
+# Write a function that counts the number of primes provided number has.
+# Ex: 5 -> 4 [1,2,3,5]
 
-end
+def isprime(number)
+  
+    idx = number - 1
+    while idx > 1
+      if number % idx == 0
+        return false
+      end
+      
+      idx = idx - 1
+    end
+    
+    return true
+  end
+  
+  def prime_count(number2)
+    
+    count = 0
+    
+    idx = number2
+    
+    while idx > 1
+      if isprime(idx) == true
+        count = count + 1
+      end
+      
+      idx = idx - 1
+    end
+    
+    return count
+      
+  end    
+  
+  puts(prime_count(9) == 4)
+  puts(prime_count(1) == 0)
+  puts(prime_count(20) == 8)
 
 
+  #Given a string, check if it's symetric
 
+# elle == true
+# henry == false
+
+
+def symmetrical(string)
+  
+    idx = 0
+    while idx < string.length
+      opp_idx = string.length - 1 - idx
+      
+      if string[idx] != string[opp_idx]
+        return false
+      end
+      
+      idx = idx + 1
+    end
+    
+    return true
+    
+  end
+  
+  puts(symmetrical("elle") == true)
+  
+  puts(symmetrical("abcdedcba") == true)
+  
+  puts(symmetrical("henry") == false)
